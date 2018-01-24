9cc1e42b0ae3ff3cc5a1932af2b20cf2138fe802
Drew Presson <drewpresson@outlook.com>
2018-01-24 16:56:31 -0600

    Updated for the game

--- GitTest2/Program.cs
+++ GitTest2/Program.cs
@@ -10,6 +10,36 @@ namespace GitTest2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("I have a number between 0 and 100....so guess it!");
+            Random randNumGen = new Random();
+            int randNum = randNumGen.Next(0, 100);
+            int userGuess = 0;
+            bool correct = false;
+            while(correct == false)
+            {
+                int guesses = 0;
+                Console.WriteLine("Enter your number");
+                int guess;
+                guess = int.Parse(Console.ReadLine());
+                if(guess == randNum)
+                {
+                    Console.WriteLine("You guessed correct!");
+                    Console.WriteLine("Your guesses " + guesses + "!");
+                    Console.ReadLine();
+                    correct = true;
+                }
+                if(guess > randNum)
+                {
+                    Console.WriteLine("Your guess is too high!");
+                    guesses++;
+                }
+                else
+                {
+                    Console.WriteLine("Your guess is too low!");
+                    guesses++;
+                }
+            }
+
         }
     }
 }
